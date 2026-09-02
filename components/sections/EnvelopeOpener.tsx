60923dbadcc3d678f36bbdf15006d7073c610a32
Sanjaya Perera <sanjayaperera80@gmail.com>
2026-09-03 02:59:36 +0530

    Update background music to Lana Del Rey - Young and Beautiful

--- components/sections/EnvelopeOpener.tsx
+++ components/sections/EnvelopeOpener.tsx
@@ -13,7 +13,7 @@ export default function EnvelopeOpener({ onOpen }: EnvelopeOpenerProps) {
   useEffect(() => {
     if (isOpening) {
       // Play subtle music
-      const audio = new Audio('/RIC-SOUND-MUSIC-Dear-lord.mp3');
+      const audio = new Audio('/young-and-beautiful.mp3');
       audio.volume = 0.3;
       audio.play().catch(() => {
         // Audio might not be available or user hasn't interacted with page
